b84c2899bc9ca94a5a6b0a0aa62eabeca3f5c1ae
jesus sanchez <jdsanchez3d@gmail.com>
2026-04-08 11:41:00 -0500

    hola

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-export function saludar (nombre){
-export function saludar(nombre){
-return "hola " + nombre + ," bienvenido a aplicacion codeRider";{
+export function saludar(nombre) {
+  return "hola " + nombre + ", bienvenido a aplicacion codeRider";
 }
-export function despedir(nombre){
-return "hasta pronto " + nombre + ," !Jesus";{ 
+
+export function despedir(nombre) {
+  return "hasta pronto " + nombre + ", !Jesus";
 }
-export function estadoSistema(){
-return {
-estado: "activo"
-version: "1.0.0"
-message: "Sistema chucho funcionando corretamente"
-};
+
+export function estadoSistema() {
+  return {
+    estado: "activo",
+    version: "1.0.0",
+    message: "Sistema chucho funcionando correctamente"
+  };
+}
+
+export function sumar(a, b) {
+  return a + b;
 }
