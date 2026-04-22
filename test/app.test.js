605fd1d464b7b88456520bbed14a3a2b67d53264
jesus sanchez <jdsanchez3d@gmail.com>
2026-04-22 10:30:16 -0500

    fix: mover test y limpiar archivo

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,9 @@
-import { saludar, despedir, estadoSistema, sumar, healthCheck, factorial } from "../src/app.js";
+import { saludar, estadoSistema, sumar, healthCheck, factorial } from "../src/app.js";
 
 function ejecutarPruebas() {
   let pasadas = 0;
   let fallidas = 0;
 
-  
   const r1 = saludar("rider");
   if (r1.includes("rider")) {
     console.log("test 1 pasado: funcion saludar correcta");
@@ -14,7 +13,6 @@ function ejecutarPruebas() {
     fallidas++;
   }
 
-  
   const actualEstado = estadoSistema();
   if (actualEstado.estado === "activo") {
     console.log("test 2 pasado: estadoSistema correcto");
@@ -24,38 +22,33 @@ function ejecutarPruebas() {
     fallidas++;
   }
 
-  
   const resultadoSuma = sumar(2, 3);
   if (resultadoSuma === 5) {
     console.log("test 3 pasado: suma correcta");
-    pasadas += 3; // suma 3 buenas
+    pasadas++;
   } else {
     console.log("test 3 fallido:", resultadoSuma);
-    fallidas += 1; // suma 1 mala
+    fallidas++;
   }
 
-   const health = healthCheck();
+  const health = healthCheck();
+  if (health.status === "ok") {
+    console.log("test healthCheck pasado");
+    pasadas++;
+  } else {
+    console.log("test healthCheck fallido", health);
+    fallidas++;
+  }
 
-if (health.status === "ok") {
-  console.log("test healthCheck pasado");
+  const resultadoFactorial = factorial(5);
+  if (resultadoFactorial === 120) {
+    console.log("test factorial pasado");
+    pasadas++;
+  } else {
+    console.log("test factorial fallido:", resultadoFactorial);
+    fallidas++;
+  }
 
-} else {
-  console.log("test healthCheck fallidas", health);
-  fallidas++;
-}
-const resultadoFactorial = factorial(5);
-
-if (resultadoFactorial === 120) {
-  console.log("test factorial pasado");
-  pasadas++;
-} else {
-  console.log("test factorial fallido:", resultadoFactorial);
-  fallidas++;
-}
-
-  
-
-  // Resultados
   console.log("\nResultados: " + pasadas + " pasadas, " + fallidas + " fallidas");
 
   if (fallidas > 0) process.exit(1);
